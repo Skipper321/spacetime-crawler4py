57db4c95873be993f79a598ca5496fc335f745ec
Axel Wennerlund <axel.wennerlund@gmail.com>
2025-10-22 20:12:00 -0700

    Added user agents and MacOS configurations

--- launch.py
+++ launch.py
@@ -5,6 +5,9 @@ from utils.server_registration import get_cache_server
 from utils.config import Config
 from crawler import Crawler
 
+# THESE LINES ARE NEEDED IF NOT RUNNING ON LINUX
+# import multiprocessing
+# multiprocessing.set_start_method("fork")
 
 def main(config_file, restart):
     cparser = ConfigParser()
